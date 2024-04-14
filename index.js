f871e22ec2ad490aa6637e03123ba2d0fc381328
Trevezan <b39s@hotmail.com>
2024-04-14 16:03:53 -0300

    feat: "testing POST method"

--- index.js
+++ index.js
@@ -2,11 +2,21 @@ import express from "express";
 
 const app = express();
 app.use(express.static("public"));
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
   res.render("index");
+  res.send("formulário recebido");
+});
+
+app.post("/", (req, res) => {
+  var title = req.body.title;
+  var description = req.body.description;
+  res.render("index");
+  res.send(console.log(title, description));
 });
 
 app.listen(8080, () => {
