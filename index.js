eeb3ba3ab7d44d40fa5b4a904fe2b95901d10dd0
Trevezan <b39s@hotmail.com>
2024-04-14 15:24:36 -0300

    feat: "First tests"

--- index.js
+++ index.js
@@ -1,9 +1,19 @@
-const express = require("express");
+import express from "express";
 
 const app = express();
+app.use(express.json());
 
-app.get("", (req, res) => {
-  res.send("Bem vindo ao meu site!");
+app.set("view engine", "ejs");
+
+app.get("/", (req, res) => {
+  var title = "Work";
+  var description = "8am - 14pm";
+  res.render("index", {
+    title: title,
+    description: description,
+  });
 });
 
-app.listen(8080);
+app.listen(8080, () => {
+  console.log("App working!");
+});
