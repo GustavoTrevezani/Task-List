28d4138853777e46b8719d2c70c18f147dfb5f31
Trevezan <b39s@hotmail.com>
2024-04-14 15:52:54 -0300

    feat: "home page"

--- index.js
+++ index.js
@@ -1,17 +1,12 @@
 import express from "express";
 
 const app = express();
-app.use(express.json());
+app.use(express.static("public"));
 
 app.set("view engine", "ejs");
 
 app.get("/", (req, res) => {
-  var title = "Work";
-  var description = "8am - 14pm";
-  res.render("index", {
-    title: title,
-    description: description,
-  });
+  res.render("index");
 });
 
 app.listen(8080, () => {
