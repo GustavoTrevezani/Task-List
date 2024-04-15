e35dc2ca38aa14a013420c72795ed4ba2144b331
Trevezan <b39s@hotmail.com>
2024-04-14 22:05:11 -0300

    feat: "add create task"

--- index.js
+++ index.js
@@ -15,15 +15,45 @@ try {
 }
 
 app.get("/", (req, res) => {
-  res.render("index");
-  res.send("formulário recebido");
+  Task.findAll({ raw: true, order: [["id", "DESC"]] }).then((taskToMake) => {
+    res.render("index", {
+      taskToMake: taskToMake,
+    });
+  });
 });
 
-app.post("/", (req, res) => {
+app.get("/newtask", (req, res) => {
+  res.render("createTask");
+});
+
+app.post("/savetask", (req, res) => {
   var title = req.body.title;
   var description = req.body.description;
-  res.render("index");
-  res.send(console.log(title, description));
+  Task.create({
+    title: title,
+    description: description,
+  }).then(() => {
+    res.redirect("/");
+  });
+});
+
+app.post("/delete", (req, res) => {
+  var id = req.body.id;
+  if (id != undefined) {
+    if (!isNaN(id)) {
+      Task.destroy({
+        where: {
+          id: id,
+        },
+      }).then(() => {
+        res.redirect("/");
+      });
+    } else {
+      res.redirect("/");
+    }
+  } else {
+    res.redirect("/");
+  }
 });
 
 app.listen(8080, () => {
